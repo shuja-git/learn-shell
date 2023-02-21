cc64fc877547da1a24a6e08ec167da9362764af8
shuja <shuja.gmail.com>
2023-02-21 15:12:20 +0530

    special variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,3 +5,5 @@ echo "date is ${date}"
 
 add=$((10+20))
 echo "addition of 2 number is ${add}"
+
+
---
+++ 04-input.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "first argument " $1
+echo "Second argument" $2
+echo "Script name" $0
+echo "All the argument" $*
+echo "Number of arguments" $#
+
+
