39820f132bfc1edfc785b35786c9528f2880528a
shuja <shuja.gmail.com>
2023-02-22 14:52:53 +0530

    accessing variable

--- 05-function.sh
+++ 05-function.sh
@@ -6,9 +6,11 @@ xyz() {
   echo -e "\e[31m All Argument\e[0m" $*
   echo -e "\e[31m Number of  Argument\e[0m" $#
   echo -e "\e[31m Function Name\e[0m" $0
+  echo -e "\e[34m value of b is \e[0m" ${b}
   a=100
 }
 # we can declare values in function and can be accessed from main program and vice-versa
 
 xyz 123 456
 echo ${a}
+b=200
