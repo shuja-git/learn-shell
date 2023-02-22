b17de3185fd95c095789aea36cbed46fbb5b4bd3
shuja <shuja.gmail.com>
2023-02-22 14:49:30 +0530

    values declared in function

--- 05-function.sh
+++ 05-function.sh
@@ -6,6 +6,8 @@ xyz() {
   echo -e "\e[31m All Argument\e[0m" $*
   echo -e "\e[31m Number of  Argument\e[0m" $#
   echo -e "\e[31m Function Name\e[0m" $0
+  a=100
 }
-
+# we can declare values in function and can be accessed from main program and vice-versa
+echo ${a}
 xyz 123 456
