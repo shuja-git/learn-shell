823d71199d60a9965a0e700e943793c145a47e64
shuja <shuja.gmail.com>
2023-02-22 14:46:23 +0530

    function arguments

--- 05-function.sh
+++ 05-function.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 xyz() {
-  echo -e "\e[1;32m Hello from function\e[0m"
+  echo -e "\e[31m First Argument\e[0m" $1
+  echo -e "\e[31m second Argument\e[0m" $2
+  echo -e "\e[31m All Argument\e[0m" $*
+  echo -e "\e[31m Number of  Argument\e[0m" $#
+  echo -e "\e[31m Function Name\e[0m" $0
 }
 
-xyz
+xyz 123 456
